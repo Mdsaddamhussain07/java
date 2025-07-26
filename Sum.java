9cf033d36c0fb5b15b9b26462b7c04ba0858ca06
Md Saddam Hussain <mdsaddamhussain0720@gmail.com>
2025-07-27 01:39:08 +0530

    add my all file

---
+++ Sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // int num1 = input.nextInt();
+        // int num2 = input.nextInt();
+
+        // int sum = num1 + num2;
+        // System.out.println("Sum = "+ sum);
+
+        // byte a = 40;
+        // byte b = 50;
+        // byte c =100;
+        // int d = a*b/c;
+        // System.out.println(d);
+
+    
+
+
+    }
+    
+}
